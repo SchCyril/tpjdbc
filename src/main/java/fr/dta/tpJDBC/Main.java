40aea8a36e8389ecefd998bbbde4b8c864255ee6
Cyril <schneidenbach.cyril@gmail.com>
2019-01-09 17:29:07 +0100

    tp jdbc

---
+++ src/main/java/fr/dta/tpJDBC/Main.java
@@ -0,0 +1,70 @@
+package fr.dta.tpJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+		try {
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			Statement stmt = conn.createStatement();
+
+			stmt.executeUpdate("drop table if exists Book cascade");
+			stmt.executeUpdate("drop table if exists Client cascade");
+			stmt.executeUpdate("drop table if exists BookAchete cascade");
+
+			stmt.executeUpdate(
+					"create table Book(id serial primary key, title varchar(255) NOT NULL, author varchar(255))");
+			stmt.executeUpdate(
+					"create table Client(id serial primary key, lastname varchar(255) NOT NULL, firstname varchar(255) NOT NULL, gender varchar(1), favbook int, constraint fk_favbook foreign key (favbook) references Book(id))");
+			stmt.executeUpdate(
+					"create table BookAchete(id_client int not null, id_book int not null, primary key(id_client, id_book),"
+							+ " Constraint fk_client foreign key (id_client) references Client(id), Constraint fk_book foreign key (id_book) references Book(id))");
+			
+			
+			Book b1 = new Book("Bonjour", "aaaaaaaaa");
+			Book b2 = new Book("Arthas", "Cyril");
+			Book b3 = new Book("Au revoir", "bbbbbbbbb");
+			Traitement.addBook(b1);
+			Traitement.addBook(b2);
+			Traitement.addBook(b3);
+			
+			Client c1 = new Client("Payan", "Benjamin", "M", b2.getId());
+			Client c2 = new Client("Mattera", "Lorick", "M", b3.getId());
+			Client c3 = new Client("Desplat", "Matthieu", "M", b1.getId());
+			Traitement.addClient(c1);
+			Traitement.addClient(c2);
+			Traitement.addClient(c3);
+			
+			
+			Traitement.achatClient(b1, c1);
+			Traitement.achatClient(b2, c1);
+			Traitement.achatClient(b1, c2);
+			
+			Traitement.livreParClient(c1);
+			Traitement.clientsParLivre(b1);
+			//Traitement.achatClient(b3, c3);
+		
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+
+	}
+
+}
---
+++ src/main/java/fr/dta/tpJDBC/Traitement.java
@@ -0,0 +1,136 @@
+package fr.dta.tpJDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Traitement {
+	public static void addBook(Book b) {
+
+		PreparedStatement insertion;
+
+		try {
+			String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			insertion = conn.prepareStatement("INSERT INTO Book(title, author) VALUES(?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			insertion.setString(1, b.getTitle());
+			insertion.setString(2, b.getAuthor());
+			insertion.executeUpdate();
+
+			ResultSet generatedKeys = insertion.getGeneratedKeys();
+			generatedKeys.next();
+			int id = generatedKeys.getInt("id");
+			b.setId(id);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void addClient(Client c) {
+		PreparedStatement addClient;
+		try {
+			String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			addClient = conn.prepareStatement(
+					"insert into Client(lastname, firstname, gender, favbook) values(?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			addClient.setString(1, c.getLastname());
+			addClient.setString(2, c.getFirstname());
+			addClient.setString(3, c.getGender());
+
+			addClient.setInt(4, c.getFavbook());
+
+			addClient.executeUpdate();
+
+			ResultSet generatedKeysClient = addClient.getGeneratedKeys();
+			generatedKeysClient.next();
+			int idClient = generatedKeysClient.getInt("id");
+			c.setId(idClient);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void achatClient(Book b, Client c) {
+		PreparedStatement achatClient;
+		try {
+			String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			achatClient = conn.prepareStatement("insert into Bookachete(id_client, id_book) values(?, ?)");
+
+			achatClient.setInt(1, c.getId());
+			achatClient.setInt(2, b.getId());
+
+			achatClient.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void livreParClient(Client c) {
+
+		try {
+			String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			PreparedStatement livreParClient = conn.prepareStatement(
+					"select title, author from book join bookachete on book.id = bookachete.id_book where bookachete.id_client = ?");
+
+			livreParClient.setInt(1, c.getId());
+
+			ResultSet resultSet = livreParClient.executeQuery();
+
+			while (resultSet.next()) {
+				
+				String title = resultSet.getString("title");
+				String author = resultSet.getString("author");
+				System.out.println(title + " " + author);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void clientsParLivre(Book b) {
+
+		try {
+			String url = "jdbc:postgresql://localhost:5432/tpJDBC";
+			Connection conn = DriverManager.getConnection(url, "postgres", "azerty");
+			PreparedStatement clientsParLivre = conn.prepareStatement(
+					"select lastname, firstname from client join bookachete on client.id = bookachete.id_client where bookachete.id_book = ?");
+
+			clientsParLivre.setInt(1, b.getId());
+
+			ResultSet resultSet = clientsParLivre.executeQuery();
+
+			while (resultSet.next()) {
+				System.out.println(b.getTitle());
+				String nom = resultSet.getString("lastname");
+				String prenom = resultSet.getString("firstname");
+				System.out.println(nom + " " + prenom);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+}
